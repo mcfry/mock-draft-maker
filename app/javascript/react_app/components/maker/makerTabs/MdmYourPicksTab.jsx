47ca2e900a0a23bf03eff040190d8f3f6ad74297
Mcfry <b.j.mcfetridge@gmail.com>
2024-02-12 20:29:47 -0500

    change 'your draft' tab to be comprehensive

--- app/javascript/react_app/components/maker/makerTabs/MdmYourPicksTab.jsx
+++ app/javascript/react_app/components/maker/makerTabs/MdmYourPicksTab.jsx
@@ -1,39 +1,55 @@
 // External
-import React, { Fragment } from "react"
+import React, { useMemo, Fragment } from "react"
+import useStore from "../../../store/store"
 
 // Internal
-import NoData from "../../helpers/NoData"
+// import NoData from "../../helpers/NoData"
 
 function MdmYourPicksTab({ yourPicks }) {
+  const selected = useStore(state => state.selected);
+  const teams = useStore(state => state.teams);
+  const pickData = useStore(state => state.pickData);
+
+  const selectedTeams = useMemo(() => teams.filter(team => team.id in selected), [teams])
+
+  console.log(yourPicks)
+
   return (
     <>
       <div className="flex flex-col dark:bg-gray-300 dark:text-gray-900">
-        {yourPicks && Object.keys(yourPicks).length > 0 ? (
-          <div className="overflow-x-auto w-[62rem] h-[31rem]">
-            <table className="table font-mono text-base">
-              <tbody>
-                {Object.entries(yourPicks).map(([team, players]) => (
-                  <Fragment key={`yp_${team}`}>
-                    <tr>
-                      <th>{team}</th>
-                    </tr>
-                    {players.map(player => (
-                      <tr key={`yps_${player.id}`}>
-                        <td>Projection: {player.projected}</td>
+        <div className="overflow-x-auto w-[62rem] h-[34.75rem]">
+          {selectedTeams?.map(team => 
+            <div className="flex flex-col justify-center items-center">
+              <div className="flex items-center justify-center text-xl font-bold py-2 w-full border-t-2 border-b-2">
+                <span>{team.full_name}</span>
+              </div>
+              <table className="table font-mono text-base table-auto">
+                <tbody>
+                  <Fragment key={`yp_${team.full_name}`}>
+                    {yourPicks[team.full_name]?.map(player => {
+                      return <tr key={`yps_${player.pickedAt}`}>
                         <td>Pick: {player.pickedAt}</td>
                         <td>{player.full_name}</td>
                         <td>{player.position}</td>
                         <td>{player.college}</td>
+                        <td>Projection: {player.projected}</td>
                       </tr>
-                    ))}
+                    })}
+                    {pickData[team.full_name]?.map(pick => {
+                      return <tr key={`yps_${pick}`}>
+                        <td>Pick: {pick}</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                      </tr>
+                    })}
                   </Fragment>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        ) : (
-          <NoData message={"You haven't picked any players yet."} />
-        )}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
       </div>
     </>
   )
